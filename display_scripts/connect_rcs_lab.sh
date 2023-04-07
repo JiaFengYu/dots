0fd64c23a87bbf206136e9c365cd1b1be003b6bf
JiaFengYu <JiaFengYu@github.com>
2023-04-07 12:28:17 -0400

    Updated .bashrc aliases

---
+++ display_scripts/connect_rcs_lab.sh
@@ -0,0 +1,4 @@
+xrandr --output eDP-1 --off --output DP-1 --mode 3840x2160 --rate 60.00 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off
+xrandr --output DP-1 --scale 1.00x1.00
+feh --bg-fill /home/jiafengyu/Wallpapers/real_moon.jpg
+picom 
