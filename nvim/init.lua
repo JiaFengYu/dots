be240bbb80a1157e1ec24fe3dc3d5ce6ded21526
JiaFengYu <JiaFengYu@github.com>
2023-04-26 01:21:22 -0400

    Updated nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,9 +5,18 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
     Plug "jdhao/better-escape.vim"
     Plug "rust-lang/rust.vim"
     Plug "lervag/vimtex"
+    --Plug "-hui/fidget.nvim"
+    Plug "hrsh7th/cmp-buffer"
+    Plug "hrsh7th/cmp-path"
+    Plug "hrsh7th/cmp-cmdline"
+    Plug "hrsh7th/nvim-cmp"
+    Plug "hrsh7th/cmp-nvim-lsp"
+    Plug "simrat39/rust-tools.nvim"
+    Plug "hrsh7th/vim-vsnip"
+    Plug 'junegunn/fzf'
 -- vim.cmd('Plug "morhetz/gruvbox"')
 -- vim.cmd('Plug "ghifarit53/tokyonight-vim"')
--- vim.cmd('Plug "vim-airline/vim-airline"')
+    Plug "vim-airline/vim-airline"
 vim.call('plug#end')
 
 vim.opt.number = true
@@ -40,6 +49,108 @@ vim.opt.scrolloff = 10
 vim.opt.sidescrolloff = 10
 -- let g:python_highlight_space_errors = 0
 -- vim.opt.termguicolors = true
+
+
+-- LSP CONFIGS 
+local lspconfig = require('lspconfig')
+local lsp_defaults = lspconfig.util.default_config
+
+lsp_defaults.capabilities = vim.tbl_deep_extend(
+  'force',
+  lsp_defaults.capabilities,
+  require('cmp_nvim_lsp').default_capabilities()
+)
+
+-- Set completeopt to have a better completion experience
+-- :help completeopt
+-- menuone: popup even when there's only one match
+-- noinsert: Do not insert text until a selection is made
+-- noselect: Do not auto-select, nvim-cmp plugin will handle this for us.
+vim.o.completeopt = "menuone,noinsert,noselect"
+
+-- Avoid showing extra messages when using completion
+vim.opt.shortmess = vim.opt.shortmess + "c"
+
+local function on_attach(client, buffer)
+  -- This callback is called when the LSP is atttached/enabled for this buffer
+  -- we could set keymaps related to LSP, etc here.
+end
+
+-- Configure LSP through rust-tools.nvim plugin.
+-- rust-tools will configure and enable certain LSP features for us.
+-- See https://github.com/simrat39/rust-tools.nvim#configuration
+local opts = {
+  tools = {
+    runnables = {
+      use_telescope = true,
+    },
+    inlay_hints = {
+      auto = true,
+      show_parameter_hints = false,
+      parameter_hints_prefix = "",
+      other_hints_prefix = "",
+    },
+  },
+
+  -- all the opts to send to nvim-lspconfig
+  -- these override the defaults set by rust-tools.nvim
+  -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+  server = {
+    -- on_attach is a callback called when the language server attachs to the buffer
+    on_attach = on_attach,
+    settings = {
+      -- to enable rust-analyzer settings visit:
+      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+      ["rust-analyzer"] = {
+        -- enable clippy on save
+        checkOnSave = {
+          command = "clippy",
+        },
+      },
+    },
+  },
+}
+
+require("rust-tools").setup(opts)
+
+-- Setup Completion
+-- See https://github.com/hrsh7th/nvim-cmp#basic-configuration
+local cmp = require("cmp")
+cmp.setup({
+  preselect = cmp.PreselectMode.None,
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    -- Add tab support
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    ["<Tab>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    }),
+  },
+
+  -- Installed sources
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "vsnip" },
+    { name = "path" },
+    { name = "buffer" },
+  },
+})
+
+
+
+
 
 -- VIMTEX CONFIG
 -- Viewer options: One may configure the viewer either by specifying a built-in
